9dc6bc9555a2c23b3123c42b17653b2c7647a5d3
Ceyhun Melek <ceyhunulasmelek@gmail.com>
2020-05-21 20:34:02 +0300

    Minor Changes

--- src/VideoPage.js
+++ src/VideoPage.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import Player from "./Player";
 
+import * as firebase from "firebase";
+import CONFIG from './firebaseConfig';
+
 class VideoPage extends React.Component {
+    state = {
+        videoTime: 0
+    }
+
+    componentDidMount() {
+        firebase.initializeApp(CONFIG);
+        firebase.database().ref('videos/' + this.props.match.params.id + '/').once('value').then(function(snapshot) {
+            console.log(snapshot)
+        });
+
+    }
+
+    testVideo = {
+        poster: "https://shaka-player-demo.appspot.com/assets/poster.jpg",
+        mpd: "https://storage.googleapis.com/shaka-demo-assets/tos-ttml/dash.mpd"
+    }
+
+    videoTimeHandler = (time) => {
+        this.setState({videoTime:time})
+    }
+
     render() {
         return(
             <div className="container">
                 <div className="row">
-                    <Player/>
+                    <Player timeHandler={this.videoTimeHandler} video={this.testVideo}/>
+                </div>
+                <div className="row">
+                    {this.state.videoTime}
                 </div>
             </div>)
     }
