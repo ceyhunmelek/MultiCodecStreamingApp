6f9f8550c8c281719eda9473aea5b47420dde670
Ceyhun Melek <ceyhunulasmelek@gmail.com>
2020-05-29 18:39:12 +0300

    Profit Chart boilerplate has created

--- src/ProfitChart.js
+++ src/ProfitChart.js
@@ -1,14 +1,101 @@
 import React from 'react';
-import Chart from 'chartjs'
+import {ResponsiveLine} from "@nivo/line";
 import 'shaka-player/dist/controls.css'
 
 class ProfitChart extends React.Component {
 
+    state = {
+        data : [{
+            "id": "vp9",
+            "color": "hsl(331, 70%, 50%)",
+            "data": [
+                {
+                    "x": "plane",
+                    "y": 49
+                },
+                {
+                    "x": "helicopter",
+                    "y": 213
+                },
+                {
+                    "x": "boat",
+                    "y": 118
+                }
+            ]
+        }]
+    };
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log(this.props.chartData)
+    }
+
     render() {
         return (
-            <div>
-                {this.props.videoTime}
-            </div>)
+            <div style={{width:'100%',height:'300px'}}>
+
+                <ResponsiveLine
+                    data={this.state.data}
+                    margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+                    xScale={{ type: 'point' }}
+                    yScale={{ type: 'linear', min: 'auto', max: 'auto', stacked: true, reverse: false }}
+                    axisTop={null}
+                    axisRight={null}
+                    axisBottom={{
+                        orient: 'bottom',
+                        tickSize: 5,
+                        tickPadding: 5,
+                        tickRotation: 0,
+                        legend: 'transportation',
+                        legendOffset: 36,
+                        legendPosition: 'middle'
+                    }}
+                    axisLeft={{
+                        orient: 'left',
+                        tickSize: 5,
+                        tickPadding: 5,
+                        tickRotation: 0,
+                        legend: 'count',
+                        legendOffset: -40,
+                        legendPosition: 'middle'
+                    }}
+                    colors={{ scheme: 'nivo' }}
+                    pointSize={10}
+                    pointColor={{ theme: 'background' }}
+                    pointBorderWidth={2}
+                    pointBorderColor={{ from: 'serieColor' }}
+                    pointLabel="y"
+                    pointLabelYOffset={-12}
+                    useMesh={true}
+                    legends={[
+                        {
+                            anchor: 'bottom-right',
+                            direction: 'column',
+                            justify: false,
+                            translateX: 100,
+                            translateY: 0,
+                            itemsSpacing: 0,
+                            itemDirection: 'left-to-right',
+                            itemWidth: 80,
+                            itemHeight: 20,
+                            itemOpacity: 0.75,
+                            symbolSize: 12,
+                            symbolShape: 'circle',
+                            symbolBorderColor: 'rgba(0, 0, 0, .5)',
+                            effects: [
+                                {
+                                    on: 'hover',
+                                    style: {
+                                        itemBackground: 'rgba(0, 0, 0, .03)',
+                                        itemOpacity: 1
+                                    }
+                                }
+                            ]
+                        }
+                    ]}
+                />
+
+            </div>
+        )
     }
 }
 
